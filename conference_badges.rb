200d5e25b988a39ceba52695458301747822ec05
Leah Boyd <leahboyd08@gmail.com>
2019-03-21 17:32:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,16 +14,16 @@ end
 
 def assign_rooms(speaker)
   speaker.each_with_index.collect do |speaker, index|
-    puts "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
+    return "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
   end
 end
 
 def printer(speakers)
   batch_badge_creator(speakers).each do |badge|
-    puts badge
+    return badge
   end
     assign_rooms(speakers).each do |assignment|
-      puts assignment
+      return assignment
     end
 end
 
