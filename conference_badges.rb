7f5221a2b614490913138713bfb00a2e4196b1dd
Leah Boyd <leahboyd08@gmail.com>
2019-03-21 17:30:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,7 +20,7 @@ def assign_rooms(speaker)
 end
 
 def printer(speakers)
-  batch_badge_creator(speakers). each do |badge|
+  batch_badge_creator(speakers).each do |badge|
     puts badge
   end
     assign_rooms(speakers).each do |assignment|
