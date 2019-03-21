8d81daf16e9b31f6c649b439da416e6f87dd8c41
Leah Boyd <leahboyd08@gmail.com>
2019-03-21 17:31:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,9 @@
 # Write your code here.
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
-
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 
 def batch_badge_creator(speakers)
